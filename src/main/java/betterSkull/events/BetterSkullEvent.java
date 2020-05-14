e358900f524c611757a96693ff44ff9861fb5973
nferris165 <51347535+nferris165@users.noreply.github.com>
2020-05-13 21:48:54 -0400

    event overhaul
    added scaling hp costs
    added alternate gold costs
    added new options text
    retooled button options
    separated metrics from content

--- src/main/java/betterSkull/events/BetterSkullEvent.java
+++ src/main/java/betterSkull/events/BetterSkullEvent.java
@@ -17,6 +17,8 @@ import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.apache.commons.lang3.math.NumberUtils.max;
+
 public class BetterSkullEvent extends AbstractImageEvent {
 
     public static final String ID = BetterSkull.makeID("BetterSkull");
@@ -30,16 +32,16 @@ public class BetterSkullEvent extends AbstractImageEvent {
     private static final String REFUTE_MSG;
     private static final String INTRO_2_MSG;
     private static final String ASK_AGAIN_MSG;
-    private static final String POTION_MSG;
-    private static final String CARD_MSG;
+    private static final String UPGRADE_MSG;
+    private static final String RELIC_MSG;
     private static final String GOLD_MSG;
     private static final String LEAVE_MSG;
     private int healAmt;
-    private int potionCost;
-    private int cardCost;
+    private int relicCost;
+    private int upgradeCost;
     private int goldCost;
     private int leaveCost;
-    private static final int GOLD_REWARD = 90;
+    private int goldReward;
     private CurScreen screen;
     private String optionsChosen;
     private int damageTaken;
@@ -53,8 +55,10 @@ public class BetterSkullEvent extends AbstractImageEvent {
 
         if (AbstractDungeon.ascensionLevel >= 15) {
             this.healAmt = (int)(AbstractDungeon.player.maxHealth * 0.05F);
+            this.goldReward = 75;
         } else {
             this.healAmt = (int)(AbstractDungeon.player.maxHealth * 0.1F);
+            this.goldReward = 100;
         }
 
         this.screen = CurScreen.INTRO_1;
@@ -66,10 +70,10 @@ public class BetterSkullEvent extends AbstractImageEvent {
         this.options.add(Reward.GOLD);
         this.options.add(Reward.POTION);
         this.options.add(Reward.LEAVE);
-        this.leaveCost = 6;
-        this.cardCost = this.leaveCost;
-        this.potionCost = this.leaveCost;
-        this.goldCost = this.leaveCost;
+        this.leaveCost = 1;
+        this.upgradeCost = max((int)(AbstractDungeon.player.maxHealth * 0.1F), 6);
+        this.relicCost = max((int)(AbstractDungeon.player.maxHealth * 0.2F), 6);
+        this.goldCost = max((int)(AbstractDungeon.player.maxHealth * 0.1F), 6);
         this.damageTaken = 0;
         this.goldEarned = 0;
         this.cards = new ArrayList<>();
@@ -91,12 +95,7 @@ public class BetterSkullEvent extends AbstractImageEvent {
                 switch(buttonPressed) {
                     case 0:
                         this.imageEventText.updateBodyText(INTRO_2_MSG);
-                        this.imageEventText.clearAllDialogs();
-                        this.imageEventText.setDialogOption(OPTIONS[4] + this.potionCost + OPTIONS[1]);
-                        this.imageEventText.setDialogOption(OPTIONS[5] + 90 + OPTIONS[6] + this.goldCost + OPTIONS[1]);
-                        this.imageEventText.setDialogOption(OPTIONS[3] + this.cardCost + OPTIONS[1]);
-                        this.imageEventText.setDialogOption(OPTIONS[7] + this.leaveCost + OPTIONS[1]);
-                        this.screen = CurScreen.ASK;
+                        this.setChoices();
                         return;
                     case 1:
                         this.imageEventText.updateBodyText(REFUTE_MSG);
@@ -112,13 +111,36 @@ public class BetterSkullEvent extends AbstractImageEvent {
                 CardCrawlGame.sound.play("DEBUFF_2");
                 switch(buttonPressed) {
                     case 0:
-                        this.obtainReward(0);
+                        AbstractDungeon.player.damage(new DamageInfo(null, this.goldCost, DamageInfo.DamageType.HP_LOSS));
+                        ++this.goldCost;
+                        this.imageEventText.updateBodyText(GOLD_MSG + ASK_AGAIN_MSG);
+                        AbstractDungeon.effectList.add(new RainingGoldEffect(goldReward));
+                        AbstractDungeon.player.gainGold(goldReward);
+                        this.setChoices();
+                        //Metrics
+                        this.damageTaken += this.goldCost;
+                        this.optionsChosen = this.optionsChosen + "GOLD ";
+                        this.goldEarned += goldReward;
                         return;
                     case 1:
-                        this.obtainReward(1);
+                        AbstractDungeon.player.damage(new DamageInfo(null, this.upgradeCost, DamageInfo.DamageType.HP_LOSS));
+                        ++this.upgradeCost;
+                        this.imageEventText.updateBodyText(UPGRADE_MSG + ASK_AGAIN_MSG);
+                        //TODO
+                        this.setChoices();
+                        //Metrics
+                        this.damageTaken += this.upgradeCost;
+                        this.optionsChosen = this.optionsChosen + "UPGRADE ";
                         return;
                     case 2:
-                        this.obtainReward(2);
+                        AbstractDungeon.player.damage(new DamageInfo(null, this.relicCost, DamageInfo.DamageType.HP_LOSS));
+                        ++this.relicCost;
+                        this.imageEventText.updateBodyText(RELIC_MSG + ASK_AGAIN_MSG);
+                        //TODO
+                        this.setChoices();
+                        //Metrics
+                        this.damageTaken += this.relicCost;
+                        this.optionsChosen = this.optionsChosen + "RELIC ";
                         return;
                     default:
                         AbstractDungeon.player.damage(new DamageInfo(null, this.leaveCost, DamageInfo.DamageType.HP_LOSS));
@@ -137,65 +159,27 @@ public class BetterSkullEvent extends AbstractImageEvent {
 
     }
 
-    private void obtainReward(int slot) {
-        switch(slot) {// 143
-            case 0:
-                AbstractDungeon.player.damage(new DamageInfo((AbstractCreature)null, this.potionCost, DamageInfo.DamageType.HP_LOSS));// 145
-                this.damageTaken += this.potionCost;// 146
-                ++this.potionCost;// 147
-                this.optionsChosen = this.optionsChosen + "POTION ";// 148
-                this.imageEventText.updateBodyText(POTION_MSG + ASK_AGAIN_MSG);// 149
-                if (AbstractDungeon.player.hasRelic("Sozu")) {// 150
-                    AbstractDungeon.player.getRelic("Sozu").flash();// 151
-                } else {
-                    AbstractPotion p = PotionHelper.getRandomPotion();// 153
-                    this.potions.add(p.ID);// 154
-                    AbstractDungeon.player.obtainPotion(p);// 155
-                }
-                break;
-            case 1:
-                AbstractDungeon.player.damage(new DamageInfo((AbstractCreature)null, this.goldCost, DamageInfo.DamageType.HP_LOSS));// 159
-                this.damageTaken += this.goldCost;// 160
-                ++this.goldCost;// 161
-                this.optionsChosen = this.optionsChosen + "GOLD ";// 162
-                this.imageEventText.updateBodyText(GOLD_MSG + ASK_AGAIN_MSG);// 163
-                AbstractDungeon.effectList.add(new RainingGoldEffect(90));// 164
-                AbstractDungeon.player.gainGold(90);// 165
-                this.goldEarned += 90;// 166
-                break;// 167
-            case 2:
-                AbstractDungeon.player.damage(new DamageInfo((AbstractCreature)null, this.cardCost, DamageInfo.DamageType.HP_LOSS));// 169
-                this.damageTaken += this.cardCost;// 170
-                ++this.cardCost;// 171
-                this.optionsChosen = this.optionsChosen + "CARD ";// 172
-                this.imageEventText.updateBodyText(CARD_MSG + ASK_AGAIN_MSG);// 173
-                AbstractCard c = AbstractDungeon.returnColorlessCard(AbstractCard.CardRarity.UNCOMMON).makeCopy();// 174
-                this.cards.add(c.cardID);// 175
-                AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(c, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));// 176
-                break;// 178
-            default:
-                BetterSkull.logger.info("This should never happen.");
-        }
-
-        this.imageEventText.clearAllDialogs();// 183
-        this.imageEventText.setDialogOption(OPTIONS[4] + this.potionCost + OPTIONS[1]);// 184
-        this.imageEventText.setDialogOption(OPTIONS[5] + 90 + OPTIONS[6] + this.goldCost + OPTIONS[1]);// 185
-        this.imageEventText.setDialogOption(OPTIONS[3] + this.cardCost + OPTIONS[1]);// 186
-        this.imageEventText.setDialogOption(OPTIONS[7] + this.leaveCost + OPTIONS[1]);// 187
-    }// 188
+    private void setChoices(){
+        this.imageEventText.clearAllDialogs();
+        this.imageEventText.setDialogOption(OPTIONS[5] + goldReward + OPTIONS[6] + this.goldCost + OPTIONS[1]);
+        this.imageEventText.setDialogOption(OPTIONS[3] + this.upgradeCost + OPTIONS[1]);
+        this.imageEventText.setDialogOption(OPTIONS[4] + this.relicCost + OPTIONS[1]);
+        this.imageEventText.setDialogOption(OPTIONS[7] + this.leaveCost + OPTIONS[1]);
+        this.screen = CurScreen.ASK;
+    }
 
     private void setLeave() {
-        this.imageEventText.updateBodyText(LEAVE_MSG);// 191
-        this.imageEventText.clearAllDialogs();// 192
-        this.imageEventText.setDialogOption(OPTIONS[8]);// 193
-        this.screen = CurScreen.COMPLETE;// 194
+        this.imageEventText.updateBodyText(LEAVE_MSG);
+        this.imageEventText.clearAllDialogs();
+        this.imageEventText.setDialogOption(OPTIONS[8]);
+        this.screen = CurScreen.COMPLETE;
     }
 
     static {
         INTRO_2_MSG = DESCRIPTIONS[1];
         ASK_AGAIN_MSG = DESCRIPTIONS[2];
-        POTION_MSG = DESCRIPTIONS[4];
-        CARD_MSG = DESCRIPTIONS[5];
+        UPGRADE_MSG = DESCRIPTIONS[4];
+        RELIC_MSG = DESCRIPTIONS[5];
         GOLD_MSG = DESCRIPTIONS[6];
         LEAVE_MSG = DESCRIPTIONS[7];
         REFUTE_MSG = DESCRIPTIONS[8];
